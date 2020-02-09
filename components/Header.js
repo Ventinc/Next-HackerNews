7163a1ec6e03fa23d3bee53828064c507af0b321
Vincent Dusautoir <ventinc@Vincents-MacBook-Pro.local>
2020-02-09 10:03:51 +0100

    Feat: Add Header

---
+++ components/Header.js
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styled from "styled-components";
+import colors from "../config/colors";
+
+const HeaderContainer = styled.div`
+  position: fixed;
+  background-color: ${colors.yellow500};
+  width: 100%;
+  line-height: 48px;
+  color: #FFFFFF;
+`
+
+const HeaderContent = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+  }
+`
+
+const HeaderLogo = styled.h1`
+  font-size: 1.2em;
+  margin: 0;
+  color: ${colors.yellow700};
+  margin-right: 20px;
+`
+
+const HeaderLinks = styled.ul`
+  font-size: 1em;
+  color: ${colors.yellow700};
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  
+  li {
+    display: inline;
+    margin: 0px 8px;
+    a {
+      color: ${colors.yellow300};
+      text-decoration: none;
+    }
+
+    &.active {
+      a {
+        color: ${colors.yellow100};
+      }
+    }
+  }
+`
+
+function HeaderLink({ href, children }) {
+  const router = useRouter();
+
+  const active = router.pathname === href;
+
+  return <li className={active ? 'active' : null}><Link href={href}><a>{children}</a></Link></li>
+}
+
+function Header() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <HeaderLogo>HackerNextJS</HeaderLogo>
+        <HeaderLinks>
+          <HeaderLink href='/'>Top</HeaderLink>
+        </HeaderLinks>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import NextHead from 'next/head';
 import styled from "styled-components";
 import colors from "../config/colors";
+import Header from './Header';
 
 const LayoutContainer = styled.div`
   background-color: ${colors.gray200};
@@ -10,8 +11,9 @@ const LayoutContainer = styled.div`
 const Container = styled.div`
   width: 800px;
   margin: 0px auto;
-  min-height: calc(100vh - 60px);
-  padding: 30px 0px;
+  min-height: calc(100vh - 100px);
+  padding-top: 80px;
+  padding-bottom: 20px;
 
   @media screen and (max-width: 800px) {
     width: 100%;
@@ -25,6 +27,7 @@ const Layout = ({ children }) => (
       <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet" />
     </NextHead>
     <LayoutContainer>
+      <Header />
       <Container>
         {children}
       </Container>
